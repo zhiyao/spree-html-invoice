45dc9d957495af4545797bd8c1e992656a4ad006
Enrique Alvarez <enriqueaf@gmail.com>
2013-08-22 17:58:28 +0200

    Add adjustments variable

--- app/controllers/spree/admin/invoice_controller.rb
+++ app/controllers/spree/admin/invoice_controller.rb
@@ -5,6 +5,7 @@ module Spree
         template = params[:template]
         eval "@#{template} = true"
         @order = Spree::Order.find_by_number(params[:id])
+        @adjustments = @order.adjustments.eligible.where('amount>?',0)
         render  template , :layout => false
       end
     end
