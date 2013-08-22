6e1e13a4acf0a6eaf51f1f6f2d1fa454bc7c9574
Enrique Alvarez <enriqueaf@gmail.com>
2013-08-22 12:26:54 +0200

    Change text so they are variable

--- lib/spree/html_invoice_configuration.rb
+++ lib/spree/html_invoice_configuration.rb
@@ -2,6 +2,8 @@ module Spree
   class HtmlInvoiceConfiguration < Preferences::Configuration
     preference :html_invoice_logo_path, :string
     preference :html_receipt_logo_path, :string
+    preference :html_receipt_info, :string
+    preference :html_receipt_email, :string
     preference :print_buttons, :string
     preference :suppress_anonymous_address, :boolean, :default => true
   end
